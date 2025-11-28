5d859def83afe3d7bee6f4803261f57ef28df3ec
Hoorror555 <joaorama.eng@gmail.com>
2025-11-28 22:56:27 +0000

    pequena alteracao

--- veiculo.c
+++ veiculo.c
@@ -1,36 +1,21 @@
 #include "Settings.h"
 
 int main(int argc, char * argv[]){
-  printf("Hello! Sou o veiculo.\n");
+  int kms = 0;
+
 
   if (argc!=2){
     perror("[sistema] Numero invalido de parametros!\n");
     exit(-1);
   }
 
-  if (access(SERVERFIFO, F_OK) == 0) {
-    printf("[sistema] O servidor ja se encontra em execucao\n");
-    exit(0);
+  /* RECEBE POR ARGUMENTO O LOCAL QUE É O MAX DE DISTANCIA A PERCORRER AQUI SERIA UM ARGV METI ARGV[3] mas pode se outro
+  while(km!= argv[3]){
+    ++km;
+    sleep (1);
   }
+  */
 
-  int fd = open(SERVERFIFO, O_RDWR);
-    //verificar se abriu o pipe
-  if(fd < 0){
-    perror("Erro ao abrir pipe do servidor!\n");
-    exit(-1);
-  }
 
-  if (mkfifo(VEICULOFIFO, 0666) == -1) {
-    if (errno != EEXIST) {
-    perror("[sistema] Erro na criacao do named pipe");
-    exit(EXIT_FAILURE);
-    }
-  }
-
-  while(1){
-    scanf("");
-  }
-  close(fd);
-  //unlink(VEICULOFIFO);
   return 0;
 }
